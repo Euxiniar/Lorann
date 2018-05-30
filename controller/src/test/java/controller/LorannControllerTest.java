9f2e6451c920a586059c6d12b9288973e19b19fa
Trapadore <vaz.vicente.mc@gmail.com>
2018-05-30 22:57:56 +0200

    Ajout test du LorannController

---
+++ controller/src/test/java/controller/LorannControllerTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author vazvi
+ *
+ */
+public class LorannControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link controller.LorannController#buildMap(String)}.
+	 */
+	@Test
+	public void testBuildMap() {
+		fail("Not yet implemented");
+	}
+
+}
