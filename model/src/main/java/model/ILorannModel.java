c427fcf79a103aad311993ad861d994dec12a104
Euxiniar <charles.ago37@gmail.com>
2018-05-30 18:02:07 +0200

    Revert "Revert "reupdate""
    This reverts commit af7de76eaa6331cf29eb0573008d9209c725f2e2.

--- model/src/main/java/model/ILorannModel.java
+++ model/src/main/java/model/ILorannModel.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * <h1>The ILorannModel.java Class.</h1>
  *
@@ -27,3 +28,34 @@ public interface ILorannModel {
 	ArrayList<IMonster> getMonsters();
 	void addMonster(IMonster monster);
 }
+=======
+/**
+ * <h1>The ILorannModel.java Class.</h1>
+ *
+ * @author Charles Agostini
+ * @version 1.0
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import model.element.mobile.IMonster;
+import model.element.mobile.Player;
+import model.element.mobile.Spell;
+
+/**
+ * @author Euxiniar
+ *
+ */
+public interface ILorannModel {
+	Map getMap();
+	void setMap(Map map);
+	Player getPlayer();
+	void setPlayer(Player player);
+	Spell getSpell();
+	void setSpell(Spell spell);
+	ArrayList<IMonster> getMonsters();
+	void addMonster(IMonster monster);
+}
+>>>>>>> parent of af7de76... Revert "reupdate"
