a3296bd7968a8957e3de1cf663b82f975cfec47b
Euxiniar <charles.ago37@gmail.com>
2018-06-04 21:29:48 +0200

    try fix test error in travis

--- view/src/test/java/GameFrame/GameFrameTest.java
+++ view/src/test/java/GameFrame/GameFrameTest.java
@@ -2,20 +2,26 @@ package GameFrame;
 
 import static org.junit.Assert.*;
 
+import java.util.Observable;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+@SuppressWarnings("deprecation")
 public class GameFrameTest {
 
 	private boolean[] orderBools; 
 	private static GameFrame gameFrame;
+	static Observable observable = new Observable();
+	static IGraphicsBuilder graphicsBuilder;
+	
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		gameFrame = new GameFrame(null, null, null);
+		gameFrame = new GameFrame("Something", graphicsBuilder, observable);
 	}
 
 	@AfterClass
